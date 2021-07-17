d09e95ce63ed1ef1f1efb9a3234d7fd86a283206
mgbarbero <mgbarbero@gmail.com>
2021-07-16 21:40:46 -0300

    Update init_data.sh

--- install-storage/init_data.sh
+++ install-storage/init_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Populating characters table"
-oc exec deployment.apps/postgresql-persistent -i redhat123 -- /usr/bin/psql -U redhat persistentdb < /home/student/DO280/labs/install-storage/init_data.sql
+oc exec deployment.apps/postgresql-persistent -i redhat123 -- /usr/bin/psql -U redhat persistentdb < ~/ocp4labs/install-storage/init_data.sql
 
